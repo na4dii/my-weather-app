8fea410e1c4c7a40637f34f3da51ba6900f33e71
na4dii <87880823+na4dii@users.noreply.github.com>
2021-09-13 19:50:32 -0400

    Updated default city
    Changed from New York to Philadelphia

--- src/app.js
+++ src/app.js
@@ -127,4 +127,4 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector(".location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("New York");
+searchCity("Philadelphia");
