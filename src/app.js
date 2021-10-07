777cc6bfa3b6d35e8eef235864b1af2aec08c750
na4dii <87880823+na4dii@users.noreply.github.com>
2021-10-07 18:36:01 -0400

    Updated URL

--- src/app.js
+++ src/app.js
@@ -81,7 +81,7 @@ function showWeather(response) {
   sunsetElement.innerHTML = formatDate(response.data.sys.sunset * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
