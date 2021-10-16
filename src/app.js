bce1343bab97748c233369e12f67a71a2ec11f56
na4dii <87880823+na4dii@users.noreply.github.com>
2021-10-15 23:34:41 -0400

    Updated HTML, JS & CSS for additional current weather parameters
    Changed time from 24hr to 12 hr with local time offset for sunrise and sunset.
    
    Replaced precipitation and pollen count with visibility and cloudiness

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ let apiKey = "66fbe743ffda6d29ce514fb34af10af8";
 let apiMain = `https://api.openweathermap.org/data/2.5/weather?`;
 
 let now = new Date();
+let localOffset = now.getTimezoneOffset() * 60;
 
 let daysOfWeek = [
   "Sunday",
@@ -36,20 +37,21 @@ let todaysDate = document.querySelector(".currentDate");
 todaysDate.innerHTML = `${dayOfWeek}, ${month} ${day}`;
 
 function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
+  let date = new Date(timestamp * 1000);
 
-  if (hours < 10) {
-    hours = `0${hours}`;
+  let amPM = `am`;
+  let hours = date.getHours();
+  if (hours >= 12) {
+    amPM = "pm";
   }
+  hours = hours % 12 || 12;
 
   let minutes = date.getMinutes();
-
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
-  return `${hours}:${minutes}`;
+  return `${hours}:${minutes} ${amPM}`;
 }
 
 function formatDay(timestamp) {
@@ -91,7 +93,7 @@ function displayForecast(response) {
                     forecastDay.weather[0].icon
                   }@2x.png"
                   alt=""
-                  class="weather-icon"
+                  class="weather-img"
                 /></li>
                 <li><span class="forecast-description">${
                   forecastDay.weather[0].description
@@ -131,12 +133,14 @@ function showWeather(response) {
   let sunsetElement = document.querySelector("#sunset");
   let timeElement = document.querySelector(".currentTime");
   let iconElement = document.querySelector(".weather-icon");
+  let cloudElement = document.querySelector("#cloudy");
+  let visibleElement = document.querySelector("#visibility");
 
   fahrenheitTemp = response.data.main.temp;
   fahrenheitTempHigh = response.data.main.temp_max;
   fahrenheitTempLow = response.data.main.temp_min;
 
-  timeElement.innerHTML = formatDate(response.data.dt * 1000);
+  timeElement.innerHTML = formatDate(response.data.dt);
   h1.innerHTML = `Current Weather in ${response.data.name}`;
   currentTemperatureElement.innerHTML = Math.round(fahrenheitTemp);
   currentHighElement.innerHTML = Math.round(fahrenheitTempHigh) + "°";
@@ -144,8 +148,14 @@ function showWeather(response) {
   currentDescription.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
   windElement.innerHTML = Math.round(response.data.wind.speed) + " mph";
-  sunriseElement.innerHTML = formatDate(response.data.sys.sunrise * 1000);
-  sunsetElement.innerHTML = formatDate(response.data.sys.sunset * 1000);
+  sunriseElement.innerHTML = formatDate(
+    response.data.sys.sunrise + localOffset + response.data.timezone
+  );
+  sunsetElement.innerHTML = formatDate(
+    response.data.sys.sunset + localOffset + response.data.timezone
+  );
+  cloudElement.innerHTML = `${response.data.clouds.all}%`;
+  visibleElement.innerHTML = `${Math.round(response.data.visibility * .000621)} mi`;
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
